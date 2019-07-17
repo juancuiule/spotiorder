8dfcad2f87c30a5f328c51a1febb58b1fe1ae3ac
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2019-07-17 01:26:02 -0300

    fetchs

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,126 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { TOKEN } from './config';
+
+import { Playlist, Track, AudioFeautre } from './types';
+import { string } from 'prop-types';
+
 const App: React.FC = () => {
+  const [username, setUsername] = React.useState('');
+
+  const [playlists, setPlaylists] = React.useState<Playlist[]>([]);
+  const [tracks, setTracks] = React.useState<Track[]>([]);
+
+  const search = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const { items } = (await fetch(
+      `https://api.spotify.com/v1/users/${username}/playlists?limit=50`,
+      {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${TOKEN}`
+        }
+      }
+    ).then(r => r.json())) as { items: Playlist[]; next: string };
+    setPlaylists(items);
+  };
+
+  const searchTracks = (href: string) => async () => {
+    const { items } = (await fetch(href, {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${TOKEN}`
+      }
+    }).then(r => r.json())) as { items: Track[] };
+    setTracks(items);
+  };
+
+  const [orderedTracks, setOrderedTracks] = React.useState<string[]>([])
+
+  const getAudioFeatures = React.useCallback(async () => {
+    const ids = tracks.map(t => t.track.id).join(',');
+    const { audio_features } = (await fetch(
+      `https://api.spotify.com/v1/audio-features?ids=${ids}`,
+      {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${TOKEN}`
+        }
+      }
+    ).then(r => r.json())) as { audio_features: AudioFeautre[] };
+
+    const zip = tracks.map((t, i) => ({
+      track: t,
+      audio_feature: audio_features[i]
+    }))
+
+    const ordered = zip.sort((a, b) => {
+      return a.audio_feature.tempo - b.audio_feature.tempo
+    }).map(x => x.track.track.name)
+
+    setOrderedTracks(ordered)
+  }, [tracks]);
+
+  React.useEffect(() => {
+    getAudioFeatures();
+  }, [tracks, getAudioFeatures]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <p>Search by username</p>
+        <form onSubmit={search}>
+          <input
+            autoFocus
+            type="text"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+          />
+          <button>Search</button>
+        </form>
+        <div
+          style={{
+            width: '80%',
+            margin: '15px auto',
+            display: 'flex',
+            flexDirection: 'row',
+            flexWrap: 'wrap'
+          }}
         >
-          Learn React
-        </a>
+          {tracks.length === 0
+            ? playlists.map(p => {
+                return (
+                  <div onClick={searchTracks(p.tracks.href)}>
+                    <img
+                      src={p.images[0].url}
+                      style={{
+                        height: '100px',
+                        width: '100px',
+                        margin: '3px'
+                      }}
+                    />
+                  </div>
+                );
+              })
+            : null}
+          <ul style={{ fontSize: '10px', textAlign: 'left' }}>
+            {tracks.map(t => (
+              <li>{t.track.name}</li>
+            ))}
+          </ul>
+          <ul style={{ fontSize: '10px', textAlign: 'left' }}>
+            {orderedTracks.map(t => (
+              <li>{t}</li>
+            ))}
+          </ul>
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/types.ts
@@ -0,0 +1,72 @@
+export type Image = {
+  height: number;
+  url: string;
+  width: number;
+};
+
+export interface Playlist {
+  collaborative: boolean;
+  external_urls: {
+    spotify: string;
+  };
+  href: string;
+  id: string;
+  images: Image[];
+  name: string;
+  owner: {
+    display_name: string;
+    external_urls: {
+      spotify: string;
+    };
+    href: string;
+    id: string;
+    type: string;
+  };
+  public: boolean;
+  snapshot_id: string;
+  tracks: {
+    href: string;
+    total: number;
+  };
+  type: string;
+  uri: string;
+}
+
+export interface Track {
+  added_at: string
+  added_by: {
+    external_urls: {
+      spotify: string
+    }
+    href: string
+    id: string
+    type: string
+    uri: string
+  }
+  is_local: boolean
+  track: {
+    id: string
+    name: string
+  }
+}
+
+export interface AudioFeautre {
+  analysis_url: string
+  id: string
+  track_href: string
+  type: string
+  uri: string
+  acousticness: number
+  danceability: number
+  duration_ms: number
+  energy: number
+  instrumentalness: number
+  key: number
+  liveness: number
+  loudness: number
+  mode: number
+  speechiness: number
+  tempo: number
+  time_signature: number
+  valence: number
+}
