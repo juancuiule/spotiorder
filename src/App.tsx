e6f766fa7391e5d2503226af75b885fd600345de
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2019-07-21 23:01:18 -0300

    hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { lazy, Suspense } from "react";
 
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import { getHashParams, login } from "./utils/spotify_auth";
 import API from "./utils/api";
 
